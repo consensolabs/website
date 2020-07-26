a7b359bf9648fee6a642936cc02ccb0aafd09cd5
abhinav anshul <abhinavanshul4u@gmail.com>
2020-07-26 21:41:56 +0530

    adds Footer

---
+++ tailblocks/src/components/Technology.js
@@ -0,0 +1,70 @@
+import React from 'react'
+// import ReactLogo from '../assets/logo/ReactLogo.svg'
+
+//const ReactLogoStyle = <img style={{ width: '20%' }} src={ReactLogo} />
+
+function Technology() {
+  return (
+    <>
+      <div>
+        <h1 className='flex justify-center sm:text-3xl text-2xl font-medium title-font mb-4 py-6 '>
+          Technology Stack We Work On
+        </h1>
+
+        <div
+          style={{
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr 1fr',
+            backgroundColor: 'pink',
+            textAlign: 'center',
+            gridGap: '1rem',
+            paddingLeft: '12rem',
+            paddingRight: '12rem',
+            paddingTop: '1rem',
+            paddingBottom: '1rem',
+            marginBottom: '4rem',
+          }}
+        >
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* {ReactLogoStyle} */}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Technology
