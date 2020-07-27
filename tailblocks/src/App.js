0f9b111ebd0a04ccc7b4bc93aa2d1329801b3121
abhinav anshul <abhinavanshul4u@gmail.com>
2020-07-27 16:57:55 +0530

    adds linting

--- tailblocks/src/App.js
+++ tailblocks/src/App.js
@@ -4,8 +4,8 @@ import Hero from './components/Hero'
 import Description from './components/Description'
 import Solution from './components/Solution'
 import Technology from './components/Technology'
-import Footer from './components/Footer'
-function App() 
+import Footer from "./components/Footer"
+function App() {
   return (
     <>
       <Header />
@@ -13,7 +13,6 @@ function App()
       <Description />
       <Solution />
       <Technology />
-      <br />
       <Footer />
     </>
   )
