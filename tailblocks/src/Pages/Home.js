e6deed6c6f7df569a3c8b0f0a30becd5bacbc20a
koushith <koushith97@gmail.com>
2020-07-27 18:32:14 +0530

    added about page

--- tailblocks/src/Pages/Home.js
+++ tailblocks/src/Pages/Home.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Description from '../components/Description';
 import Solution from '../components/Solution';
-// import Technology from './components/Technology';
-// import Footer from './components/Footer';
-import Nav from '../components/Nav';
+import Technology from '../components/Technology';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Description />
       <Solution />
+      <Technology />
+      <br />
+      <Footer />
     </>
   );
 };
