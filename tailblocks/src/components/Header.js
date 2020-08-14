c4c27ea243f26f79735a5b615953cd2ade6600a7
abhinav anshul <abhinavanshul4u@gmail.com>
2020-08-14 13:21:30 +0530

    replaces Header logo

--- tailblocks/src/components/Header.js
+++ tailblocks/src/components/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HeaderLogo from '../assets/logo/HeaderLogo.png'
+
+const HeaderLogoStyle = <img src={HeaderLogo}/>
 
 function Header() {
   return (
@@ -11,19 +14,9 @@ function Header() {
             href
             className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0'
           >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              stroke='currentColor'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth='2'
-              className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full'
-              viewBox='0 0 24 24'
-            >
-              <path d='M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5'></path>
-            </svg>
-            <span className='ml-3 text-xl'>Consenso Labs</span>
+            
+            {HeaderLogoStyle}
+            
           </Link>
           <nav className='md:ml-auto flex flex-wrap items-center text-base justify-center'>
             <Link to='/about' className='mr-5 hover:text-gray-900'>
@@ -33,7 +26,7 @@ function Header() {
               Product
             </Link>
             <Link to='#' className='mr-5 hover:text-gray-900'>
-              Platform
+              Services
             </Link>
             <Link to='/team' className='mr-5 hover:text-gray-900'>
               Teams
