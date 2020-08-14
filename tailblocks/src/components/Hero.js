cfe0974eee4e30d7387979083771e84cc6e90106
abhinav anshul <abhinavanshul4u@gmail.com>
2020-08-14 12:57:14 +0530

    updates hero section content

--- tailblocks/src/components/Hero.js
+++ tailblocks/src/components/Hero.js
@@ -8,12 +8,10 @@ function Hero() {
         <div className='container mx-auto flex px-5 py-24 md:flex-row flex-col items-center'>
           <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
             <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-textWhite'>
-              Bringing Blockchain value to the companies
+            Blockchain research, development, and services
             </h1>
             <p className='text-textWhite mb-8 leading-relaxed'>
-              Dedicated blockchain research lab to provide variety of services
-              for students, software engineers and blockchain enthusiasts. We
-              also try to address critical issues through decentralization.
+            <span style={{fontWeight : "700"}}>Consenso Labs</span> is a blockchain research and development lab where we confront ourselves to solve some of the most challenging finance, legal and economical use cases by utilizing the cutting edge technology platforms to achieve efficiency and the consensus among the disparate parties.
             </p>
             <div className='flex justify-center'>
               <button className='inline-flex text-textWhite bg-secondary border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg'>
