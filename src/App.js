7b781a0097ad06090a9815e5c311f59052ae5ba9
koushith <koushith@consensolabs.com>
2020-09-04 03:45:04 +0530

    adds mobile navigation and other UI enhancements

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Team from './Pages/Team';
 import Contact from './Pages/Contact';
 import NoMatchPage from './Pages/404';
 
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Header from './components/Header';
+import Header from './components/Navigation/Header';
 import { CookieBanner } from '@palmabit/react-cookie-law';
 function App() {
   return (
@@ -31,17 +31,17 @@ function App() {
           <Route component={NoMatchPage} />
         </Switch>
       </Router>
-  {/*    <CookieBanner*/}
-  {/*     styles={{*/}
-  {/*  dialog: { bottom : '0px' , position : "fixed", padding : '1rem', backgroundColor : "white", width : "100%" }*/}
-  {/*}}*/}
-  {/*      message="Cookie banner message"*/}
-  {/*      wholeDomain={true}*/}
-  {/*      onAccept = {() => {}}*/}
-  {/*      onAcceptPreferences = {() => {}}*/}
-  {/*      onAcceptStatistics = {() => {}}*/}
-  {/*      onAcceptMarketing = {() => {}}*/}
-  {/*    />*/}
+      {/*    <CookieBanner*/}
+      {/*     styles={{*/}
+      {/*  dialog: { bottom : '0px' , position : "fixed", padding : '1rem', backgroundColor : "white", width : "100%" }*/}
+      {/*}}*/}
+      {/*      message="Cookie banner message"*/}
+      {/*      wholeDomain={true}*/}
+      {/*      onAccept = {() => {}}*/}
+      {/*      onAcceptPreferences = {() => {}}*/}
+      {/*      onAcceptStatistics = {() => {}}*/}
+      {/*      onAcceptMarketing = {() => {}}*/}
+      {/*    />*/}
     </>
   );
 }
--- src/components/Teams/TeamsCta.js
+++ src/components/Teams/TeamsCta.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TeamsCta = () => {
   return (
-    <section className='text-gray-700 body-font'>
-      <div className='container px-5 py-24 mx-auto'>
-        <div className='lg:w-3/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto '>
-          <h1 className='flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900'>
-            Want to #buidl together? Join our team now
-          </h1>
-          <a href="https://angel.co/company/consensolabs/jobs" target="_blank">
-          <button className='flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0'>
-            Explore Roles
-          </button>
-          </a>
+    <>
+      <section className='text-gray-700 body-font  '>
+        <div className='container px-5 py-24 mx-auto'>
+          <div className='lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto mobile-center'>
+            <h1 className='flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900'>
+              Your next adventure starts here. Join our team now.
+            </h1>
+            <div className='flex justify-center mobile-center'>
+              <a
+                href='https://angel.co/company/consensolabs/jobs'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <button className='flex-shrink-0  justify-center text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0 sm:items-center '>
+                  Explore Roles
+                </button>
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 export default TeamsCta;
